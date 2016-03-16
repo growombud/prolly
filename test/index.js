c1a803b590762858e504e91970faac7a4ac1a342
Jonathan Griggs <jonathan.griggs@gmail.com>
2016-03-16 02:54:09 -0600

    Tests for sequence() with initial_value array

--- test/index.js
+++ test/index.js
@@ -7,6 +7,15 @@ describe('Prolly', () => {
     const sequence = Prolly.sequence;
     it('should evaluate a sequence of promises, in series', () => {
       const vals  = new Array(4).fill(2);
+      return sequence(vals, v => new Promise((resolve, reject) => setTimeout(() => resolve(Date.now()), v)), [Date.now()])
+        .then(results => _.reduce(results, (accu, v) => {
+          v.should.be.greaterThan(_.last(accu));
+          return accu.concat(v);
+        }, [Number.NEGATIVE_INFINITY]));
+    })
+
+    it('should evaluate a sequence of promises, in series, with an initial value', () => {
+      const vals  = new Array(4).fill(2);
       return sequence(vals, v => new Promise((resolve,reject) => setTimeout(() => resolve(Date.now()), v)))
         .then(results => _.reduce(results, (accu, v) => {
           v.should.be.greaterThan(_.last(accu));
@@ -17,6 +26,15 @@ describe('Prolly', () => {
     it('should evaluate a sequence of values, in series', () => {
       const vals  = new Array(4).fill(2);
       return sequence(vals, ( v, index ) => index )
+        .then(results => _.reduce(results, (accu, v) => {
+          v.should.be.greaterThan(_.last(accu));
+          return accu.concat(v);
+        }, [Number.NEGATIVE_INFINITY]));
+    });
+
+    it('should evaluate a sequence of values, in series, with an initial value', () => {
+      const vals  = new Array(4).fill(2);
+      return sequence(vals, ( v, index ) => index, [-1] )
         .then(results => _.reduce(results, (accu, v) => {
           v.should.be.greaterThan(_.last(accu));
           return accu.concat(v);
